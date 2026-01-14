af5c444685445a3e4eb06bd8c9f9fde72bed908d
Abdel-nasser Ben Ali <benaliabdelnasserpro@gmail.com>
2026-01-14 17:12:20 +0100

    First commit - appli démarre, DB + Flyway prêts, dépendances OK, et la structure en couches est en place

---
+++ src/main/java/com/lipa/LipaApplication.java
@@ -0,0 +1,13 @@
+package com.lipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lipa/LipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
